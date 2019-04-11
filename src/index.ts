d37932b93c017abdc5e92dd4b7bad464e9945d3b
surarin-ex <surarin_ex@icloud.com>
2019-04-11 13:44:59 +0900

    Format by Prettier+

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import * as bip39 from 'bip39';
+import * as bip39 from "bip39";
 
 /**
  * Mnemonicを生成する
  * @param {string} lang 'JA', 'EN' etc...
  */
-export function generateMnemonic(lang: string = 'EN'): string {
+export function generateMnemonic(lang: string = "EN"): string {
   if (lang) {
     bip39.setDefaultWordlist(lang);
   }
