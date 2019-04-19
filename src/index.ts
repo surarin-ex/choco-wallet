7490ca79eacb4450cdab9a0803e430077249f1e0
surarin-ex <surarin_ex@icloud.com>
2019-04-19 18:12:47 +0900

    Add  adaptive rng for various environment

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as bip39 from "bip39";
+const getRandomValues = require('get-random-values');
 
 /**
  * Mnemonicを生成する
@@ -8,5 +9,5 @@ export function generateMnemonic(lang: string = "EN"): string {
   if (lang) {
     bip39.setDefaultWordlist(lang);
   }
-  return bip39.generateMnemonic();
+  return bip39.generateMnemonic(null, getRandomValues);
 }
